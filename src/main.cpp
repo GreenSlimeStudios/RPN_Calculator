24aa0e8e41d264f7184f38397b4a33b23a094bb3
Max Berliński <maxber.gdynia@gmail.com>
2023-03-25 22:53:05 +0100

    port to st7789 display

--- src/main.cpp
+++ src/main.cpp
@@ -3,25 +3,34 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <Adafruit_ST7789.h>
 #include <Keypad.h>
 #include <vector>
 #include <bits/stdc++.h>
 
-const int SCREEN_HEIGHT = 64;
-const int SCREEN_WIDTH = 128;
+const int TFT_CS = 5;
+const int TFT_DC = 16;
+const int TFT_RST = 2;
+const int SC = 17;
+
+const int SCREEN_HEIGHT = 240;
+const int SCREEN_WIDTH = 240;
 #define OLED_RESET -1       // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+// Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+Adafruit_ST7789 display = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
 
-const byte ROWS = 4; // four rows
+const byte ROWS = 6; // four rows
 const byte COLS = 4; // three columns
 char keys[ROWS][COLS] = {
+    {'X', 'X', 'X', 'X'},
+    {'X', 'X', 'X', 'X'},
     {'F', '1', '2', '3'},
     {'S', '4', '5', '6'},
     {'A', '7', '8', '9'},
     {'J', '.', '0', 'E'}};
-byte colPins[ROWS] = {26, 18, 19, 23}; // connect to the row pinouts of the keypad
-byte rowPins[COLS] = {5, 16, 33, 17};  // connect to the column pinouts of the keypad
+byte colPins[COLS] = {14, 25, 26, 27};        // connect to the row pinouts of the keypad
+byte rowPins[ROWS] = {33, 22, 21, 4, 13, 12}; // connect to the column pinouts of the keypad
 
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 int repeat = 1;
@@ -43,28 +52,40 @@ std::string r_current = "";
 void setup()
 {
   Serial.begin(921600);
+  Serial.println(F("wha the wha"));
   // put your setup code here, to run once:
-  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
-  {
-    Serial.println(F("SSD1306 allocation failed"));
-    for (;;)
-    {
-    }
-  }
-  Serial.println(F("wha the heck"));
-  display.clearDisplay();
-  display.setTextColor(1);
+  // if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
+  // {
+  //   Serial.println(F("SSD1306 allocation failed"));
+  //   for (;;)
+  //   {
+  //   }
+  // }
+  display.init(240, 240);
+  display.setRotation(2);
+  display.fillScreen(ST77XX_BLACK);
+  display.drawRect(120, 120, 240, 240, ST77XX_BLACK);
+  display.setTextColor(ST77XX_WHITE);
+  display.setTextSize(2);
+  // for (;;)
+  // {
+  // display.print("aaaaa");
+  // Serial.println(F("wha the heck"));
+  // }
+  // display.clearDisplay();
+  // display.setTextColor(1);
   // display.println("wha the heck");
-  display.display();
+  // display.display();
 }
 void print_stack()
 {
-  display.clearDisplay();
+  // display.clearDisplay();
+  display.fillScreen(ST77XX_BLACK);
   display.setCursor(0, 0);
   int skip = 0;
-  if (stack.size() > 4)
+  if (stack.size() > 8)
   {
-    skip = stack.size() - 4;
+    skip = stack.size() - 8;
   }
 
   for (int i = skip; i < stack.size(); ++i)
@@ -88,7 +109,7 @@ void print_stack()
     }
 
     display.println(num.c_str());
-    display.drawLine(0, display.getCursorY() + 1, 128, display.getCursorY() + 1, 1);
+    display.drawLine(0, display.getCursorY() + 1, SCREEN_WIDTH, display.getCursorY() + 1, ST77XX_WHITE);
     display.setCursor(0, display.getCursorY() + 3);
   }
   // display.display();
@@ -100,7 +121,7 @@ void print_stack()
   }
 
   int cursorY = display.getCursorY();
-  display.setCursor(0, 56);
+  display.setCursor(0, SCREEN_HEIGHT - 16);
   switch (mode)
   {
   case MODE::INSERT:
@@ -126,7 +147,7 @@ void print_stack()
     display.print("r");
   }
   display.setCursor(0, cursorY);
-  display.display();
+  // display.display();
 }
 void push_to_stack()
 {
@@ -138,16 +159,17 @@ void push_to_stack()
 }
 void print_repeat_set()
 {
-  display.clearDisplay();
+  // display.clearDisplay();
+  display.fillScreen(ST77XX_BLACK);
   display.setCursor(0, 0);
   display.println("SET REPEAT");
-  display.drawLine(0, display.getCursorY() + 1, 128, display.getCursorY() + 1, 1);
+  display.drawLine(0, display.getCursorY() + 1, SCREEN_WIDTH, display.getCursorY() + 1, ST77XX_WHITE);
   display.setCursor(0, display.getCursorY() + 3);
   // display.print("R: ");
   // display.setTextColor(0, 1);
   display.print(r_current.c_str());
   // display.setTextColor(1, 0);
-  display.display();
+  // display.display();
 }
 
 void loop()
@@ -177,7 +199,10 @@ void loop()
     else if (key == 'A' && mode != MODE::REPEAT_SET)
     {
       if (mode == MODE::ALPHA)
+      {
         mode = MODE::INSERT;
+        repeat = 1;
+      }
       else
         mode = MODE::ALPHA;
       print_stack();
@@ -205,7 +230,7 @@ void loop()
         print_stack();
         // display.print(key);
         Serial.println(key);
-        display.display();
+        // display.display();
       }
       else if (mode == MODE::ALPHA)
       {
@@ -259,37 +284,37 @@ void loop()
       {
         if (key == '1' || key == '2' || key == '4' || key == '5')
         {
-          for (int i = 0; i < repeat; ++i)
+          // for (int i = 0; i < repeat; ++i)
+          // {
+          push_to_stack();
+          if (stack.size() < 1)
           {
-            push_to_stack();
-            if (stack.size() < 1)
-            {
-              mode = MODE::INSERT;
-              print_stack();
-              return;
-            }
-            long double result;
-            switch (key)
-            {
-            case '1':
-              result = sin(stack[stack.size() - 1] * PI / 180);
-              break;
-            case '2':
-              result = cos(stack[stack.size() - 1] * PI / 180);
-              break;
-            case '4':
-              result = tan(stack[stack.size() - 1] * PI / 180);
-              break;
-            case '5':
-              result = sqrt(stack[stack.size() - 1]);
-              break;
-            default:
-              break;
-            }
-            stack[stack.size() - 1] = result;
             mode = MODE::INSERT;
             print_stack();
+            return;
           }
+          long double result;
+          switch (key)
+          {
+          case '1':
+            result = sin(stack[stack.size() - 1] * PI / 180);
+            break;
+          case '2':
+            result = cos(stack[stack.size() - 1] * PI / 180);
+            break;
+          case '4':
+            result = tan(stack[stack.size() - 1] * PI / 180);
+            break;
+          case '5':
+            result = sqrt(stack[stack.size() - 1]);
+            break;
+          default:
+            break;
+          }
+          stack[stack.size() - 1] = result;
+          mode = MODE::INSERT;
+          print_stack();
+          // }
         }
         else if (key == '3' || key == '6' || key == '9' || key == 'E')
         {
